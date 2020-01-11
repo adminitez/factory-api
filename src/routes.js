787f17dbcfaf29deacb6a57cb164e43465c6bec5
Guilherme Tafelli <guihermetafelli@gmail.com>
2020-01-11 15:56:17 -0300

    test

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 const PurchaseOrderController = require('./controllers/PurchaseOrderController');
 
 routes.get("/purchase-orders", PurchaseOrderController.listPurchaseOrdersPaged);
+routes.get("/", (req, res) =>{
+    res.send("Hello World!")
+})
 
 module.exports = routes
